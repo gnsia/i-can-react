89547aaa3c0954fad69ba04b49881296799a27cf
gnsia <gnsia@naver.com>
2021-12-29 14:56:01 +0900

    backend와 통합을 위한 fetch 메소드와 Promise 전달 제작.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import React from 'react';
+import Todo from './Todo';
+import AddTodo from './AddTodo';
+import { Paper, List, Container } from "@material-ui/core";
 import './App.css';
+import { call } from "./service/ApiService";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items : [
+        {id:0, title: "hello World 1", done: true},
+        {id:1, title: "Hello World 2", done: false},
+      ],
+    };
+  }
+
+  componentDidMount() {
+    call("/todo", "GET", null).then((response) =>
+      this.setState({ items: response.data })
+    );
+  }
+
+  add = (item) => {
+    call("/todo", "POST", item).then((response) => 
+      this.setState({ items: response.data })
+    );
+  };
+
+  delete = (item) => {
+    call("/todo", "DELETE", item).then((response) =>
+      this.setState({ items: response.data })
+    );
+  };
+
+  update = (item) => {
+    call("/todo", "PUT", item).then((response) => 
+      this.setState({ items: response.data })
+    );
+  };
+
+  render() {
+    var todoItems = this.state.items.length > 0 && (
+      <Paper style={{ margin: 16}}>
+        <List>
+          {this.state.items.map((item, idx) => (
+            <Todo 
+              item={item} 
+              key={item.id} 
+              delete={this.delete}
+              update={this.update} />
+          ))}
+        </List>
+      </Paper>
+    );
+
+    return (
+      <div className="App">
+        <Container maxWidth="md">
+          <AddTodo add={this.add} />
+          <div className="TodoList">
+            {todoItems}
+          </div>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
